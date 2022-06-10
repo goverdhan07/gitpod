2cfbfa83f89c42108190cf0558db813e97386522
mustard <huiwen@gitpod.io>
2022-06-10 07:44:21 +0000

    [server,db,protocol] support ssh public key
    Co-authored-by: George Tsiolis <tsiolis.g@gmail.com>

--- components/server/src/workspace/workspace-starter.ts
+++ components/server/src/workspace/workspace-starter.ts
@@ -1356,6 +1356,8 @@ export class WorkspaceStarter {
         }
         spec.setAdmission(admissionLevel);
         spec.setVolumeSnapshot(volumeSnapshotInfo);
+        const sshKeys = await this.userDB.trace(traceCtx).getSSHPublicKeys(user.id);
+        spec.setSshPublicKeysList(sshKeys.map((e) => e.key));
         return spec;
     }
 
@@ -1389,6 +1391,10 @@ export class WorkspaceStarter {
             "function:getEnvVars",
             "function:setEnvVar",
             "function:deleteEnvVar",
+            "function:hasSSHPublicKey",
+            "function:getSSHPublicKeys",
+            "function:addSSHPublicKey",
+            "function:deleteSSHPublicKey",
             "function:trackEvent",
 
             "resource:" +
